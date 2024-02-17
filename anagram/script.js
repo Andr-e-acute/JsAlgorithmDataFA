b8cda7a5a76ea60ab063044e3f3b1039aeabb6bf
André Potthast <andre.potthast.ap@googlemail.com>
2024-02-17 14:08:41 +0100

    git should now all setup

--- anagram/script.js
+++ anagram/script.js
@@ -1,5 +1,7 @@
-function validAnagram() {
+function validAnagram(str1, str2) {
   // add whatever parameters you deem necessary - good luck!
+
+  console.log(str1);
 }
 
 validAnagram("anagram", "nagaram"); // true
